a9d9756aace549a161662e8647fad98e6a0a1fff
Rhxrr <hamzah.alhamadani@icloud.com>
2025-04-02 01:10:12 -0400

    changed the stories controller to display a more descriptive message

--- backend/src/api/storiesController.js
+++ backend/src/api/storiesController.js
@@ -1,15 +1,18 @@
-import storyModel from "../models/story.js";
+import getRandomStory from "../models/story.js";
 
-const getRandomStory = async (req, res) => {
+const storiesGET = async (req, res) => {
   try {
-    const story = await storyModel.getRandomStory();
+    const story = await getRandomStory();
     if (!story) {
-      return res.status(400).json({ message: "No stories found" });
+      return res
+        .status(400)
+        .json({ message: "Sorry we dont have any stories for you today! \n" });
     }
     res.json(story);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 };
 
-export default getRandomStory;
+export default storiesGET;
